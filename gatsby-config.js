a77cf8396fe0f403335643a637cffbac4edfecfb
deenizhan <deenizhan@users.noreply.github.com>
2023-09-21 13:58:36 +0300

    routes added

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `deenizhan`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};

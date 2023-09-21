cc7c55e462d3a0236a06d28703453055357cd081
deenizhan <deenizhan@users.noreply.github.com>
2023-09-21 13:59:52 +0300

    routes added 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page 2">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
-        alt="denden"
+        alt="denden is changed"
         src="https://dateideas.ai/_next/image?url=%2Fshowcase%2Ftemp1.webp&w=750&q=75"
       />
     </Layout>

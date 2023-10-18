d6cc5f764b30aae54d7ffa64d3bd1f120a0f4013
deenizhan <deenizhan@users.noreply.github.com>
2023-10-18 21:55:27 +0300

    moi-page-almost

--- src/pages/moi.js
+++ src/pages/moi.js
@@ -1,10 +1,8 @@
 import * as React from "react"
 import Layout from "../components/layout/layout"
 import Seo from "../components/seo/seo.js"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 import { moi } from "../styles/moi.module.css"
-import MoiCard from "../components/moiSummary/moiSummary.js"
+import MoiCard from "../components/moiCard/moiCard.js"
 
 const Moi = () => {
   return (

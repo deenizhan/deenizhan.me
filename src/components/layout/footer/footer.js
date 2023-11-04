182fab19dc0c7bd64b55bf9d6c63099c2e848eb6
deenizhan <deenizhan@users.noreply.github.com>
2023-11-04 13:50:13 +0300

    coming-soon

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
       <Line />
       <div className={footer}>
         <span className={footerTitle}>© Denizhan Yilmaz</span>
-        <span className={email}>
+        <span className={footerTitle}>
           <a href="mailto: denizhanyilmaz9@gmail.com">
             denizhanyilmaz9<span style={{ fontWeight: 800 }}>[at]</span>
             gmail.com 📪
